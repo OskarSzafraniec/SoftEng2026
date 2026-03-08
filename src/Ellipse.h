35059da4c66e71145e1f135d6b7c2a2fff73c4a2
Oskar Szafraniec <129787@uksw.edu.pl>
2026-03-08 10:52:03 +0100

    format

--- src/Ellipse.h
+++ src/Ellipse.h
@@ -8,28 +8,23 @@
 using namespace std;
 #include "ShapeParam.h"
 
-template<class T>
-class Ellipse : public Shape2D<T> {
-  public:
+template <class T> class Ellipse : public Shape2D<T> {
+public:
     inline ShapeResult<T> compute();
 
     inline string print();
 
-    inline Ellipse(const ShapeParam<T> & param);
-
+    inline Ellipse(const ShapeParam<T>& param);
 };
-template<class T>
-inline ShapeResult<T> Ellipse<T>::compute() {
-  return ShapeResult<T>();
+template <class T> inline ShapeResult<T> Ellipse<T>::compute()
+{
+    return ShapeResult<T>();
 }
 
-template<class T>
-inline string Ellipse<T>::print() {
-  return "";
-}
+template <class T> inline string Ellipse<T>::print() { return ""; }
 
-template<class T>
-inline Ellipse<T>::Ellipse(const ShapeParam<T> & param) : Shape2D<T>(param) {
-}
+template <class T>
+inline Ellipse<T>::Ellipse(const ShapeParam<T>& param): Shape2D<T>(param)
+{}
 
 #endif
